430ca87243382e9597c2cc8788b31290934f8bdf
Jack <scruffydevilliers@gmail.com>
2018-08-09 13:56:03 +0200

    Hash and Download with hash

---
+++ src/deVilliers/DownloadHasher.java
@@ -0,0 +1,120 @@
+package deVilliers;
+
+import javafx.util.Pair;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import static java.nio.file.Files.size;
+
+public class DownloadHasher {
+
+
+    public static String getFileName(URL urlInput)
+    {
+        String sout = "";
+
+        // System.out.println(FilenameUtils.getBaseName(url.getPath())); // -> file
+        //System.out.println(FilenameUtils.getExtension(url.getPath())); // -> xml
+        //System.out.println(FilenameUtils.getName(url.getPath())); // -> file.xml
+        sout = FilenameUtils.getName(urlInput.getPath());
+        return sout;
+    }
+
+    /** Create directory to save? FileUtils.moveFileToDirectory
+     * Download the file, save it, get the hash, if not permenantly storing file, delete it
+     * @param sInputLocation URL of item
+     * @param bdelete if true delete else keep
+     */
+    public static String bufferDownFullHash(String sInputLocation, boolean bdelete, int bsize)
+    {
+        ArrayList<Pair<String,String>> meta = new ArrayList<>();
+        String sout = "";
+        String sFile = "";
+        try {
+            URL website = new URL(sInputLocation);
+            sFile = getFileName(website);
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream(sFile);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        catch (Exception except)
+        {
+            System.out.println(except);
+        }
+        try
+        {
+            Hasher Hash = new Hasher();
+            sout = Hash.naiveBufferedFileReaderHash(sFile, "SHA3-256", bsize);
+
+            if (bdelete == true) {
+                File donewith = new File(sFile);
+                FileUtils.deleteQuietly(donewith);
+            }
+
+        }
+        catch (Exception exc)
+        {
+            System.out.println(exc);
+        }
+        return sout;
+    }
+
+    /** Create directory to save? FileUtils.moveFileToDirectory
+     * Download the file, save it, get the hash, if not permenantly storing file, delete it
+     * @param sInputLocation URL of item
+     */
+    public static String bufferDownFullHash(String sInputLocation)
+    {
+        ArrayList<Pair<String,String>> meta = new ArrayList<>();
+        String sout = "";
+        String sFile = "";
+        try {
+            URL website = new URL(sInputLocation);
+            sFile = getFileName(website);
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream(sFile);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        catch (Exception except)
+        {
+            System.out.println(except);
+        }
+        try
+        {
+            Hasher Hash = new Hasher();
+            sout = Hash.naiveBufferedFileReaderHash(sFile, "SHA3-256", 8192);
+            File donewith = new File(sFile);
+            FileUtils.deleteQuietly(donewith);
+
+        }
+        catch (Exception exc)
+        {
+            System.out.println(exc);
+        }
+        return sout;
+    }
+
+
+    public static long getFileSize(String sFile){
+        try {
+            Path pfilepath = Paths.get(sFile);
+            return size(pfilepath);
+        }
+        catch (IOException ioe)
+        {
+            System.out.println(ioe);
+        }
+        return 0;
+    }
+
+}
--- src/deVilliers/Main.java
+++ src/deVilliers/Main.java
@@ -2,7 +2,24 @@ package deVilliers;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+        Hasher h = new Hasher();
+        String s = h.Hash("Hello");
+        System.out.println(s);
+
+        DownloadHasher dh = new DownloadHasher();
+        System.out.println(dh.bufferDownFullHash("https://docs.oracle.com/javase/7/docs/api/java/lang/Byte.html"));
+       /* for (int i = 0; i <= 20; i++) {
+            int a = (int)Math.pow(2.0, i);
+            long p = System.currentTimeMillis();
+
+            System.out.println(a + " : " + dh.bufferDownFullHash("https://docs.oracle.com/javase/7/docs/api/java/lang/Byte.html", true, a));
+            long e = System.currentTimeMillis();
+            System.out.println("Time: " + (e-p));
+
+        }*/
+
 	// write your code here
     }
 }
